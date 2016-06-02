fb142cee9f6cc52328ea0dcef9afda3fe8774f47
Irvin Lim <irvinlimweiquan@gmail.com>
2016-06-02 18:31:25 +0800

    Added Roboto Google font

--- imports/startup/server/browser-policy.js
+++ imports/startup/server/browser-policy.js
@@ -1,5 +1,8 @@
- import { BrowserPolicy } from 'meteor/browser-policy-common';
-// e.g., BrowserPolicy.content.allowOriginForAll( 's3.amazonaws.com' );
+// Find help here:  https://themeteorchef.com/snippets/using-the-browser-policy-package/
+
+import { BrowserPolicy } from 'meteor/browser-policy-common';
 
 BrowserPolicy.content.allowImageOrigin( 'api.tiles.mapbox.com' );
+BrowserPolicy.content.allowStyleOrigin( 'fonts.googleapis.com' );
+BrowserPolicy.content.allowFontOrigin( 'fonts.gstatic.com' );
 BrowserPolicy.content.allowFontDataUrl();
