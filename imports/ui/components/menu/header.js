e1db6f6444511f57f1ffe04acfda9cff082858aa
Irvin Lim <irvinlimweiquan@gmail.com>
2016-06-10 18:35:31 +0800

    Reorganize header SCSS

--- imports/ui/components/menu/header.js
+++ imports/ui/components/menu/header.js
@@ -1,20 +1,9 @@
 import React from 'react';
-import Radium from 'radium';
 import { Navbar } from 'react-bootstrap';
 import { Link } from 'react-router';
 import DrawerNavigation from './drawer-navigation';
 
 import AppBar from 'material-ui/AppBar';
-
-var styles = {
-  titleStyle: {
-    textAlign: 'center',
-    fontSize: 20,
-    fontWeight: 400,
-    letterSpacing: 4,
-    textTransform: 'uppercase',
-  }
-};
 
 export default class Header extends React.Component {
   constructor(props) {
@@ -37,7 +26,7 @@ export default class Header extends React.Component {
   render() {
     return (
       <div id="header">
-        <AppBar title="Free4All" titleStyle={ styles.titleStyle } onLeftIconButtonTouchTap={ this.openDrawer.bind(this) } />
+        <AppBar id="header-bar" title="Free4All" onLeftIconButtonTouchTap={ this.openDrawer.bind(this) } />
         <DrawerNavigation isOpen={ this.state.drawerOpen } closeDrawer={ this.closeDrawer.bind(this) } setDrawerOpen={ this.setDrawerOpen.bind(this) } />
       </div>
     );
