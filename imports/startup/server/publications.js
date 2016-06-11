47502b5c099631845878f9103f4b087a1f771cf5
leon mak <leonmakansheng@gmail.com>
2016-06-11 20:02:30 +0800

    _ensureIndex 2dsphere for coords

--- imports/startup/server/publications.js
+++ imports/startup/server/publications.js
@@ -1,3 +1,3 @@
 import { Meteor } from 'meteor/meteor';
 import { Giveaways } from '../../api/giveaways/giveaways';
-Giveaways._ensureIndex({'coordinates':'2d'});
+Giveaways._ensureIndex({'coordinates':'2dsphere'});
