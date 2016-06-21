bc00291fb6df6df36d2c89e742f8a35a6e68f1ba
leon mak <leonmakansheng@gmail.com>
2016-06-22 07:56:24 +0800

    Added batch Id for giveaways and updated gen-data

--- imports/startup/server/data-generate.js
+++ imports/startup/server/data-generate.js
@@ -4,14 +4,14 @@ import { Categories } from '../../api/categories/categories';
 import { StatusTypes } from '../../api/status-types/status-types';
 import { Giveaways } from '../../api/giveaways/giveaways';
 import { StatusUpdates } from '../../api/status-updates/status-updates';
-
+import { shortId } from '../../util/helper'
 // Generate random markers
 const NUSLngLatBounds = [[103.772069, 1.308454], [103.785097, 1.290162]];
 const lngMin = NUSLngLatBounds[0][0];
 const lngMax = NUSLngLatBounds[1][0];
 const latMin = NUSLngLatBounds[1][1];
 const latMax = NUSLngLatBounds[0][1];
-let gaId, randomLng, randomLat, randomCat;
+let gaId, randomLng, randomLat, randomCat, randomBatchId;
 
 for (let i = 0; i < 10; i++) {
   randomLng = Math.random() * (lngMax - lngMin) + lngMin;
@@ -20,6 +20,7 @@ for (let i = 0; i < 10; i++) {
   randomCat = Categories.findOne({}, { skip: Math.floor(Math.random() * Categories.find().count()) })._id;
   randomStatus = StatusTypes.findOne({}, { skip: Math.floor(Math.random() * StatusTypes.find().count()) })._id;
   randomUser = Meteor.users.findOne({}, { skip: Math.floor(Math.random() * Meteor.users.find().count()) })._id;
+  randomBatchId = shortId.generate();
 
   gaId = Giveaways.insert({
     title:          "Giveaway #" + parseInt(i + randomIDOffset),
@@ -31,6 +32,7 @@ for (let i = 0; i < 10; i++) {
     categoryId:     randomCat,
     tags:           [ ],
     userId:         randomUser,
+    batchId:        randomBatchId,
   });
 
   StatusUpdates.insert({
