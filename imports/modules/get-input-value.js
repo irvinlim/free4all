df1a8e981737a7d1fc5799469e061c0ea8718f8a
Irvin Lim <irvinlimweiquan@gmail.com>
2016-06-22 01:19:16 +0800

    Bugfix: Fix legacy getInputValue behaviour

--- imports/modules/get-input-value.js
+++ imports/modules/get-input-value.js
@@ -2,7 +2,7 @@ import ReactDOM from 'react-dom';
 
 export const getInputValue = (component) => {
   let el = ReactDOM.findDOMNode(component);
-  if (el.tagName == "input")
+  if (el.tagName.toLowerCase() == "input")
     return el.value;
   else
     return el.getElementsByTagName("input")[0].value;
