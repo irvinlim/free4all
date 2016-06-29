ac7bad844ba5c9a6eeb97a78276c8aaed38dcbf0
leon mak <leonmakansheng@gmail.com>
2016-06-29 13:42:08 +0800

    Added timeout for geocode, rgeocode methods

--- imports/api/geocode/methods.js
+++ imports/api/geocode/methods.js
@@ -2,31 +2,45 @@ import { HTTP } from 'meteor/http'
 const geo = require('mapbox-geocoding');
 
 // Mapbox Geocoding
+let geoTimeout;
 export const geocode = (mapboxAccessToken, query, addState) => {
-	let url = 'https://api.tiles.mapbox.com/v4/geocode/mapbox.places/' + encodeURIComponent(query) + '.json?access_token=' + mapboxAccessToken;
+	let url = 'https://api.tiles.mapbox.com/geocoding/v5/mapbox.places/' + encodeURIComponent(query) 
+		+ '.json?access_token=' + mapboxAccessToken;
 	console.log(url);
-	HTTP.get(url,{},function( error, response ) {
-		if ( error ) {
-			console.log( error );
-		} else {
-			let result = JSON.parse(response.content);
-			addState(result.features);
-		}
+	if(geoTimeout) 
+		Meteor.clearTimeout(geoTimeout);
+	geoTimeout = Meteor.setTimeout( ()=>{
+		HTTP.get(url,{},function( error, response ) {
+			if ( error ) {
+				console.log( error );
+			} else {
+				let result = JSON.parse(response.content);
+				addState(result.features);
+				geoTimeout = null;
+			}
+		})
 	})
 }
 // https://api.mapbox.com/geocoding/v5/mapbox.places/-73.989,40.733.json
 // Mapbox Reverse Geocoding
+let rgeoTimeout;
 export const rgeocode = (mapboxAccessToken, latLng, openInsertDialog, removeDraggable) => {
-	let url = 'https://api.mapbox.com/geocoding/v5/mapbox.places/' + latLng.lng +','+ latLng.lat + '.json?access_token=' + mapboxAccessToken;
-	console.log(url);
-	HTTP.get(url,{},function( error, response ) {
-		if ( error ) {
-			console.log( error );
-		} else {
-			let result = JSON.parse(response.content);
-			openInsertDialog(result.features, latLng, removeDraggable);
-		}
-	})
+	let url = 'https://api.mapbox.com/geocoding/v5/mapbox.places/' + latLng.lng +','+ latLng.lat 
+		+ '.json?access_token=' + mapboxAccessToken + '&autocomplete=true';
+	if(rgeoTimeout) 
+		Meteor.clearTimeout(rgeoTimeout);
+	rgeoTimeout = Meteor.setTimeout( ()=>{
+		HTTP.get(url,{},function( error, response ) {
+			if ( error ) {
+				console.log( error );
+			} else {
+				let result = JSON.parse(response.content);
+				openInsertDialog(result.features, latLng, removeDraggable);
+				rgeoTimeout = null;
+			}
+		}) 
+	}, 1500);
+
 }
 
 
