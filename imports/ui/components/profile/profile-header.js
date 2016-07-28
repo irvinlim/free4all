8c15b81ac3761432b19e647da5c249d5c02b0816
Irvin Lim <irvinlimweiquan@gmail.com>
2016-07-28 17:23:30 +0800

    Added Profile Role chips

--- imports/ui/components/profile/profile-header.js
+++ imports/ui/components/profile/profile-header.js
@@ -1,7 +1,29 @@
 import React from 'react';
+import Chip from 'material-ui/Chip';
 
 import * as Colors from 'material-ui/styles/colors';
+import * as IconsHelper from '../../../util/icons';
 import * as UsersHelper from '../../../util/users';
+
+const makeChip = (role) => {
+  let icon, label;
+
+  if (role == "moderator") {
+    icon = "gavel";
+    label = "Mod";
+  } else if (role == "admin") {
+    icon = "android";
+    label = "Admin";
+  } else {
+    return null;
+  }
+
+  return (
+    <Chip backgroundColor="#8595b7" labelStyle={{ fontSize: 12, textTransform: 'uppercase', color: "#fff", lineHeight: "24px", padding: "0 8px" }}>
+      { label }
+    </Chip>
+  );
+};
 
 export const ProfileHeader = ({ user, shareCount, ratingPercent, homeCommunity }) => (
   <div className="profile-header flex-row">
@@ -9,7 +31,7 @@ export const ProfileHeader = ({ user, shareCount, ratingPercent, homeCommunity }
       { UsersHelper.getAvatar(user, 240, { width: "100%", height: 'auto' }) }
     </div>
     <div className="col-xs-8 col-sm-10">
-      <h1>{ UsersHelper.getFullName(user) }</h1>
+      <h1>{ UsersHelper.getFullName(user) } <span className="role-chips">{ user.roles.map(makeChip) }</span></h1>
       { homeCommunity ? <h5>{ homeCommunity.name }</h5> : null }
       <p style={{ color: Colors.grey600 }}>{ UsersHelper.getBio(user) }</p>
       <ul className="user-stats">
