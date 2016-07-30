eb04be0970851b78eb6e0431ac0c8b103a276eb8
Irvin Lim <irvinlimweiquan@gmail.com>
2016-07-31 02:00:04 +0800

    Poll for new Google APIs token every 45 minutes on the server

--- imports/startup/server/google-analytics-config.js
+++ imports/startup/server/google-analytics-config.js
@@ -2,14 +2,23 @@ GoogleAnalytics = {};
 
 const GAPI = require("gapitoken");
 const GAnalytics = require('googleanalytics');
+let pollToken;
 
 var gapi = new GAPI({
   iss: Meteor.settings.private.GoogleAnalytics.emailAddress,
   key: Meteor.settings.private.GoogleAnalytics.privateKey,
   scope: "https://www.googleapis.com/auth/analytics",
 }, function() {
+  // Set GAPI token
+  getGAPIToken();
+
+  // Refresh the GAPI token every 45 minutes
+  pollToken = setInterval(getGAPIToken, 60000 * 45);
+});
+
+const getGAPIToken = () => {
   gapi.getToken(function(err, token) {
-    console.log("Google Analytics token: " + token);
+    console.log("Refreshed Google Analytics token: " + token);
 
     if (err) {
       console.log(err);
@@ -17,4 +26,4 @@ var gapi = new GAPI({
       GoogleAnalytics = new GAnalytics.GA({ token });
     }
   });
-});
+};
