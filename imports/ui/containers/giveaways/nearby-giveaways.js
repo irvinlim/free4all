dc157d5295f6369dcc2295368fc9781c9d9e7c95
Irvin Lim <irvinlimweiquan@gmail.com>
2016-08-05 03:34:19 +0800

    Removed setTimeout & limit for nearby giveaways; fixed sorting order (can use for timeline as well?)

--- imports/ui/containers/giveaways/nearby-giveaways.js
+++ imports/ui/containers/giveaways/nearby-giveaways.js
@@ -8,17 +8,30 @@ import { Giveaways } from '../../../api/giveaways/giveaways';
 import * as LatLngHelper from '../../../util/latlng';
 
 const composer = (props, onData) => {
-  setTimeout(function() {
-    const giveaways = Giveaways.find({}, {
-      sort: { startDateTime: -1 }, // Show more recent ones first
-      limit: 10,
-    });
+  const now = new Date();
+  const nextWeek = moment(now).add(1, 'week').toDate();
 
-    onData(null, {
-      giveaways: giveaways,
-      nearbyOnClick: props.nearbyOnClick,
-    });
-  }, 500);
+  // Show ongoing ones sorted by more recently started ones first
+  // followed by upcoming ones sorted by starting soonest first
+
+  const ongoingGiveaways = Giveaways.find({
+    startDateTime:  { $lte: now },   // Start date must be earlier than now
+    endDateTime:    { $gt:  now },   // End date must be later than now
+  }, {
+    sort: { startDateTime: -1 },     // Show more recently started ones first
+  }).fetch();
+
+  const upcomingGiveaways = Giveaways.find({
+    startDateTime:  { $gt: now, $lte: nextWeek }, // Start date must be later than now, earlier than next week
+    endDateTime:    { $gt:  now },                // End date must be later than now
+  }, {
+    sort: { startDateTime: 1 },   // Show starting soonest first
+  }).fetch();
+
+  onData(null, {
+    giveaways: ongoingGiveaways.concat(upcomingGiveaways),
+    nearbyOnClick: props.nearbyOnClick,
+  });
 };
 
 export default composeWithTracker(composer, Loading)(NearbyGiveaways);
