cbc1589d338b3688bd495c49806e46857c80a2da
Irvin Lim <irvinlimweiquan@gmail.com>
2016-08-07 13:47:47 +0800

    Added actions for mods to unflag/remove flagged comments

--- imports/ui/components/giveaways/giveaway-comments.js
+++ imports/ui/components/giveaways/giveaway-comments.js
@@ -4,11 +4,14 @@ import FlatButton from 'material-ui/FlatButton';
 import { Scrollbars } from 'react-custom-scrollbars';
 import Store from '../../../startup/client/redux-store';
 
+import { pluralizer } from '../../../util/helper';
 import * as GiveawaysHelper from '../../../util/giveaways';
 import * as UsersHelper from '../../../util/users';
+import * as RolesHelper from '../../../util/roles';
 import * as LayoutHelper from '../../../util/layout';
+import * as Colors from 'material-ui/styles/colors';
 
-import { editComment, removeComment, flagComment } from '../../../api/giveaway-comments/methods';
+import { editComment, removeComment, flagComment, unflagComment, removeFlaggedComment } from '../../../api/giveaway-comments/methods';
 
 const middot = <span>&nbsp;&middot;&nbsp;</span>;
 
@@ -22,14 +25,19 @@ const CommentsList = (self, comments, owner) => (
   </Scrollbars>
 );
 
-const CommentRow = (self, owner) => (comment, index) => (
-  <div className="comment-row" key={index}>
-    { self.state.currentlyEditing && self.state.currentlyEditing == comment._id ?
-      CommentRowEditing(self, comment, owner) :
-      CommentRowDisplay(self, comment, owner)
-    }
-  </div>
-);
+const CommentRow = (self, owner) => (comment, index) => {
+  const isFlaggedAndMod = RolesHelper.modsOrAdmins(Meteor.user()) && GiveawaysHelper.countTotalFlags(comment) > 0;
+  return (
+    <div
+      className={`comment-row ${ isFlaggedAndMod ? 'comment-flagged' : '' }`}
+      key={index}>
+      { self.state.currentlyEditing && self.state.currentlyEditing == comment._id ?
+        CommentRowEditing(self, comment, owner) :
+        CommentRowDisplay(self, comment, owner)
+      }
+    </div>
+  );
+};
 
 const CommentRowDisplay = (self, comment, owner) => {
   const { _id, content, user, createdAt, updatedAt } = comment;
@@ -39,6 +47,7 @@ const CommentRowDisplay = (self, comment, owner) => {
     <div className="comment-body">
       <h5 className="comment-username">{ UsersHelper.getUserLink(user, UsersHelper.getFullNameWithLabelIfEqual(user, owner, "Author")) }</h5>
       { GiveawaysHelper.commentBody(content) }
+
       <p className="timestamp small-text">
         { updatedAt ? "updated " + moment(updatedAt).fromNow() : moment(createdAt).fromNow() }
         { self.props.showActions && Meteor.userId() ?
@@ -46,6 +55,8 @@ const CommentRowDisplay = (self, comment, owner) => {
           null
         }
       </p>
+
+      { RolesHelper.modsOrAdmins(Meteor.userId()) ? CommentActionsMod(self, comment) : null }
     </div>,
     40
   );
@@ -69,6 +80,23 @@ const CommentActionsNonOwner = (self, comment) => (
     }
   </span>
 );
+
+const CommentActionsMod = (self, comment) => {
+  const flagCount = GiveawaysHelper.countTotalFlags(comment);
+
+  if (flagCount > 0)
+    return (
+      <p className="timestamp small-text">
+        flagged { flagCount } { pluralizer(flagCount, 'time', 'times') }
+        { middot }
+        <a role="button" onTouchTap={ event => self.handleUnflagComment(comment._id) }>Unflag</a>
+        { middot }
+        <a role="button" onTouchTap={ event => self.handleUnflagComment(comment._id) } style={{ color: Colors.red700 }}>Delete comment</a>
+      </p>
+    );
+  else
+    return null;
+};
 
 const CommentRowEditing = (self, { _id, content, user, createdAt, updatedAt }, owner) => LayoutHelper.twoColumns(
   UsersHelper.getAvatar(user, 40, { margin: "0 auto", display: "block" }),
@@ -166,6 +194,22 @@ export class GiveawayComments extends React.Component {
         Bert.alert(error.reason, 'danger');
       } else {
         Bert.alert('Thanks for flagging, we will be reviewing this shortly.', 'success');
+      }
+    });
+  }
+
+  handleUnflagComment(_id) {
+    const userId = Meteor.userId();
+
+    if (!_id)
+      return false;
+
+    unflagComment.call({ _id, userId }, (error) => {
+      if (error) {
+        console.log(error);
+        Bert.alert(error.reason, 'danger');
+      } else {
+        Bert.alert('Successfully removed all flags.', 'success');
       }
     });
   }
